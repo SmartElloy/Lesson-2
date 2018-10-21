aa21c3fa0b6e2352e1164f57ac331a45f9a94aa2
SmartElloy <shalaev.gleb.andr.16@mail.ru>
2018-10-21 12:16:10 +0600

    123
    123

---
+++ script.js
@@ -0,0 +1,11 @@
+let textarea = document.getElementById("sandbox"),
+    text = textarea.value;
+button = document.getElementById("find");
+button.addEventListener("click", ShowText);
+function ShowText() {
+    text = textarea.value;
+    text = text.replace(/[,]/g, " ");
+    text = text.replace(/[.]/g, " ");
+    text = text.replace(/[!]/g, " ");
+    alert(text);
+}
